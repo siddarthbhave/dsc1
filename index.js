5c7219cfdb7c2e6f7d7bf57e1b267fb02d6bb3a4
Siddarth Bhave <siddarthbhave@gmail.com>
2019-09-26 15:07:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function log(){
+  const obj = {   a : document.getElementById("email").value,
+              b : document.getElementById("pass").value
+}
+function validateEmail(sEmail) {
+  var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+
+  if(!sEmail.match(reEmail)) {
+    alert("Invalid email address");
+    return false;
+  }
+
+  return true;
+
+}
+truthValue = validateEmail(obj.a);
+console.log(obj.a);
+
+if(truthValue){
+  location.href = "pass-in.html";
+
+}
+
+}
+ //module.exports { obj } ;
---
+++ pass-in.js
@@ -0,0 +1,3 @@
+import { obj } from 'index.js;'
+
+console.log(obj.a);
